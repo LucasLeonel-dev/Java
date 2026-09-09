0d2457bd41be571a650385e20b48ad202eb78b73
LucasLeonel-dev <lucleonelg4@gmail.com>
2026-09-09 09:14:50 -0300

    add test case

--- Test/TesteCasePilha01.java
+++ Test/TesteCasePilha01.java
@@ -38,4 +38,52 @@ class TesteCasePilha01 {
 		assertFalse(p.push(30)); //aqui 3a chamada (V2.1) e 3a asser��o (S2.2) 
 	}
 
+	@Test 
+	void test03(){ 
+		Pilha p = new Pilha(); //caso teste de valor default, V1.1
+		assertTrue(p.push(3)); // V1.1 - L2.1 (true)
+		RetornoPilha ret = p.pop(); 
+		
+		assertTrue(p.isEmpty());
+		assertTrue(ret.vazia); // S1.1 (true)
+
+		ret = p.pop();
+		assertTrue(ret.vazia); //S1.3 retorno de lista ja vazia
+
+		assertTrue(p.push(1)); // L2.1 - S2.1,limite de valores do tamanho da pilha default 
+		assertTrue(p.push(2));
+		assertTrue(p.push(3));
+		assertTrue(p.push(4));
+		assertTrue(p.push(5));
+		assertTrue(p.push(6));
+		assertTrue(p.push(7));
+		assertTrue(p.push(8));
+		assertTrue(p.push(9));
+		assertTrue(p.push(10));
+
+		assertFalse(p.push(11)); // L2.2 - S2.2, além do limite de valores no caso default
+	}
+
+	@Test 
+	void test04(){
+		Pilha p =new Pilha(3);
+
+		assertTrue(p.push(10)); //V2.1, L2.1 aceota numeros positivos
+		assertTrue(p.push(-22)); //V2.1, L2.2, aceita numeros negativos 
+	}
+	/*
+	@Test 
+	void test05(){ // teste de caso de invalidez, I2.3
+		Pilha p = new Pilha();
+		
+		assertFalse(p.push('a')); // V2.1, I2.1 - I2.2 nao aceita valores float, somente double , nem null, I2.3 porem aceita valores char
+	}
+	*/
+
+	@Test 
+	void test06(){
+		Pilha p = new Pilha(0);
+
+		assertFalse(p.push(1)); //V1.1, caso de teste para valor 0 no tamanho do pilha
+	}
 }

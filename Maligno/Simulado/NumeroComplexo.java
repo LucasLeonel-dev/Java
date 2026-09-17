685b264a878dcad85c4c31bb53d4a3894fb2ca06
Lucas Leonel <lucleonelg4@gmail.com>
2026-09-17 16:27:57 -0300

    add metodos obrigatorios in NumeroComplexo

--- Maligno/Simulado/NumeroComplexo.java
+++ Maligno/Simulado/NumeroComplexo.java
@@ -1,5 +1,7 @@
 package Simulado;
 
+import java.util.Objects;
+
 public class NumeroComplexo {
     private double num1;
     private double num2;
@@ -36,19 +38,33 @@ public class NumeroComplexo {
 
     @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
+        if (num2>=0){
+            return num1 + "+" + num2 + "i";
+        } else{
+            return num1 + "-" + Math.abs(num2) + "i";
+        }
     }
 
     @Override
     public int hashCode() {
-        // TODO Auto-generated method stub
-        return super.hashCode();
+        int retorno = 1; 
+        retorno = retorno * 7 /*numero primo pq ai nao vao ter numeros iguais*/
+        + ((Double)this.num1).hashCode(); //numero diferente dá um hash diferente, mas numero igual tem hash igual por conta de ter o this.num 
+        retorno = retorno * 7 + ((Double)this.num2).hashCode();
+        //faz com que todos fiquem com o msm hash 
+        if(retorno<0) retorno =-retorno; //se for negativo soma um numero negativo para que vire 0 
+        return retorno;
     }
 
     @Override
-    protected Object clone() throws CloneNotSupportedException {
-        // TODO Auto-generated method stub
-        return super.clone();
+    public boolean equals(Object obj) {//assinatura é Object pois se mudasse nao seria mais Override, seria criar um método novo 
+        if(this == obj){ //atalho de performance, se for o mesmo objeto na memória, já retorna true sem comparar campos
+            return true;
+        }
+        if (obj==null || this.getClass()!= obj.getClass()){ //descarta null e objetos de outra classe
+            return false; 
+        }
+        NumeroComplexo outro = (NumeroComplexo) obj; //instancia do obj(Objeto) da classe numeroComplexo 
+        return num1 == outro.num1 && num2 == outro.num2; 
     }
 }
